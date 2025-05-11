1ede47692c2103479bd5f5c4858fa23b54c014e0
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-05-11 14:42:53 -0400

    Added comments

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import 'dotenv/config';
+import 'dotenv/config'; // Enables environment variables from .env file
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
@@ -8,21 +8,23 @@ import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
+
 connectDB();
 
 const CLIENT_URL = 'http://localhost:5173';
 
+// Allows the server to accept requests from different origins
 app.use(cors({
-    origin: CLIENT_URL,
-    credentials: true,
+    origin: CLIENT_URL, // Only allow requests from CLIENT_URL
+    credentials: true,  // Allow credentials to be sent in the request
 }));
 
-app.use(cookieParser());
-app.use(express.json());
-
+app.use(cookieParser()); // Makes cookies available in requests
+app.use(express.json()); // Allows JSON to be parsed in requests
 
 app.get('/', (req, res) => res.send('API Working'));
-app.use('/api/auth', authRouter);
-app.use('/api/user', userRouter);
+
+app.use('/api/auth', authRouter); // Mount authRouter routes under /api/auth
+app.use('/api/user', userRouter); // Mount userRouter routes under /api/user
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
